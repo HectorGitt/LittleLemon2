6a252e0e0c51a5f58ecfbf0505c83616211e3d15
Hector <adeniyi.olaitanhector@yahoo.com>
2023-02-28 14:21:27 +0100

    rest framework settings

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -136,7 +136,11 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-    ]
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
 }
 
 DJOSER = {
