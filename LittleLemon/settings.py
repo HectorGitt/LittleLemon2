f31c8da6df726607e786df303a1864431dbc3f22
Hector <adeniyi.olaitanhector@yahoo.com>
2023-03-01 12:41:17 +0100

    added django filters to installed apps

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'djoser',
+    'django_filters',
     
 ]
 
@@ -143,8 +144,14 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.OrderingFilter',
+        'rest_framework.filters.SearchFilter',
+    ],
 
 }
+
 
 DJOSER = {
     'USER_ID_FIELD': 'username',
